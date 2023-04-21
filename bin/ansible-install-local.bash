0774bb04fcae4f7f8787376253f527b961f6f92b
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2023-04-21 10:08:56 +0900

    chore: install-local: Update copyright

--- bin/ansible-install-local.bash
+++ bin/ansible-install-local.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##
 ## Ansible: Setup user's local Ansible environment
-## Copyright (c) 2017-2021 SATOH Fumiyasu @ OSS Technology Corp., Japan
+## Copyright (c) 2017-2023 SATOH Fumiyasu @ OSSTech Corp., Japan
 ##
 ## License: GNU General Public License version 3
 ##
